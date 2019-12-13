c82ec1f10a95aead606a3c00ccb19c6941b4c2e8
Morgan Heinemann <heinemann.morgan@gmail.com>
2019-12-12 20:57:55 -0800

    node value change

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 document.getElementById('test').addEventListener('click', testFunc);
 
 function testFunc() {
@@ -5,7 +6,7 @@ function testFunc() {
     const level = document.getElementById('level').nodeValue;
     const name = document.getElementById('name').nodeValue;
     const newNode = document.createElement('li');
-    newNode.nodeValue = `${name} is level ${level}`;
+    newNode.innerHTML = `${name} is level ${level}`;
     
     document.getElementById('charList').appendChild(newNode);
 }
