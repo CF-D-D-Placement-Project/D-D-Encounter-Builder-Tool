584d41715b174397077012699a6c703b878c49e9
Morgan Heinemann <heinemann.morgan@gmail.com>
2019-12-12 20:26:56 -0800

    add element test

--- js/app.js
+++ js/app.js
@@ -2,4 +2,10 @@ document.getElementById('test').addEventListener('click', testFunc);
 
 function testFunc() {
     console.log('hellow wolrd');
+    const level = document.getElementById('level').nodeValue;
+    const name = document.getElementById('name').nodeValue;
+    const newNode = document.createElement('li');
+    newNode.nodeValue = `${name} is level ${level}`;
+    
+    document.getElementById('charList').appendChild();
 }
