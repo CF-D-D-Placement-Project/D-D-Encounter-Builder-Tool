fa2861be8eaaf0c01849b89a8f8f823ec5dad805
Morgan Heinemann <heinemann.morgan@gmail.com>
2019-12-12 21:24:29 -0800

    wrong value test

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ document.getElementById('test').addEventListener('click', testFunc);
 
 function testFunc() {
     console.log('hellow wolrd');
-    const level = document.getElementById('level').nodeValue;
-    const name = document.getElementById('name').nodeValue;
+    const level = document.getElementById('level').value;
+    const name = document.getElementById('name').value;
     const newNode = document.createElement('li');
     newNode.innerHTML = `${name} is level ${level}`;
     
