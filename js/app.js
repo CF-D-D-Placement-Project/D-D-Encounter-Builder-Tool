2756fb84e80c80d4359c646dfbd033c0b2c8d748
Morgan Heinemann <heinemann.morgan@gmail.com>
2019-12-14 15:59:06 -0800

    basic webpage created and reasonably appealing
    UI updates, store/retrieve dm name, add agents

--- js/app.js
+++ js/app.js
@@ -1,24 +1,67 @@
 /* eslint-env browser */
+var adventuringParty = {};
+
+document.getElementById("submitName").addEventListener('click', saveDmName);
+document.getElementById("clearName").addEventListener('click', clearDmName);
+document.getElementById("addCharacter").addEventListener('click', addCharacter);
 
 //store a user's info and party members
-if (!localStorage.getItem('AdventuringParty')) {
-    document.getElementById('userName').value = null;
+if (!localStorage.getItem('adventuringParty') || 
+    (JSON.parse(localStorage.getItem('adventuringParty'))['dmName'] == "") 
+){
+    document.getElementById('dmNameField').value == "";
+    adventuringParty = {dmName:"", party:[], settings:[]};
+    document.getElementById('dmNameField').hidden = false;
+    document.getElementById('submitName').hidden = false;
+    document.getElementById('clearName').hidden = true;
 } else {
-    const userData = JSON.parse(localStorage.AdventuringParty);
-    document.getElementById('userName').value = userData.userName;
+    adventuringParty = JSON.parse(localStorage.getItem('adventuringParty')); 
+    console.log(adventuringParty);   
+    document.getElementById('returningDmHeader').innerHTML = `Welcome Back ${adventuringParty['dmName']}`;
+    document.getElementById('dmNameField').hidden = true;
+    document.getElementById('submitName').hidden = true;
+    document.getElementById('clearName').hidden = false;
+}
+
+function saveDmName(){
+    var dmName = document.getElementById("dmNameField").value;
+    console.log('dmname:' + dmName);
+    adventuringParty['dmName'] = dmName;
+    if (dmName != ""){
+        localStorage.setItem('adventuringParty', JSON.stringify(adventuringParty));
+    }
+    let test = localStorage.getItem('adventuringParty');
+    console.log(test);
+}
+
+function clearDmName(){
+    adventuringParty["dmName"] = "";
+    localStorage.setItem('adventuringParty', JSON.stringify(adventuringParty));
+    location.reload();
+
 }
 
 //constructor to add party member to list
-document.getElementById('pcCreate').addEventListener('click', testFunc);
-
-function testFunc() {    
-    const level = document.getElementById('level').value;
-    const name = document.getElementById('name').value;
+function addCharacter() {    
+    const level = document.getElementById('pcLevel').value;
+    const pcName = document.getElementById('pcName').value;
     const newNode = document.createElement('li');
-    newNode.innerHTML = `${name} is level ${level}`;
+    newNode.className = 'character';
+    newNode.innerHTML = `${pcName} is level ${level}`;
     
-    document.getElementById('charList').appendChild(newNode);
+    var charCount = document.getElementsByClassName('character');
+    console.log(charCount);
+    if (charCount.length < 11){
+        document.getElementById('characterList').appendChild(newNode);
+    } else if (charCount.length > 10) {
+        alert('I think 10 players is enough you madman');
+    }
 }
+
+
+
+
+
 
 
 //constructor to remove party member from list
